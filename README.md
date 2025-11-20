4ce838b53d5c77824b1f49813f69ab30f2070f5c
sfgxdfdfg <murk9939@gmail.com>
2025-11-20 22:40:50 +0100

    "its working" commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Egyenlore nem muskzik!
-**DE MAJD FOG :)**
+# Szamolo gep :)
+**Eleg egyszeru de krialy :)**
+
+- Hasznalat
+    1. Irj egy szamot az elso text input-ba
+    2. Irj egy szamot a masodik text input-ba
+    3. Kattints a szamold gombra
+
+__Ennyire Egyszeru :)__
--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 var sumtext = document.getElementById("sum");
-var b1 = document.getElementById("b1");
-var b2 = document.getElementById("b2");
-var b3 = document.getElementById("b3");
-var b4 = document.getElementById("b4");
-var b5 = document.getElementById("b5");
-var b6 = document.getElementById("b6");
-var b7 = document.getElementById("b7");
-var b8 = document.getElementById("b8");
-var b9 = document.getElementById("b9");
-var b0 = document.getElementById("b0");
-var number1;
-var number2;
-var sum;
+var numberBox = document.getElementById("numberBox");
+var numberBox2 = document.getElementById("numberBox2");
+const sumButton = document.getElementById("sumButton");
 
-sumtext.innerText = number1 + number2;
+sumButton.addEventListener("click", function() {
+    var num1 = parseInt(numberBox.value, 10);
+    var num2 = parseInt(numberBox2.value, 10);
+    sumtext.innerText = (num1 + num2);
+    console.log(num1 + num2)
+});
