d1e76a7ecbb339d1428a1902409f948390e6719d
sfgxdfdfg <murk9939@gmail.com>
2025-11-20 20:20:11 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var sumtext = document.getElementById("sum");
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var b3 = document.getElementById("b3");
+var b4 = document.getElementById("b4");
+var b5 = document.getElementById("b5");
+var b6 = document.getElementById("b6");
+var b7 = document.getElementById("b7");
+var b8 = document.getElementById("b8");
+var b9 = document.getElementById("b9");
+var b0 = document.getElementById("b0");
+var number1;
+var number2;
+var sum;
+
+sumtext.innerText = number1 + number2;
